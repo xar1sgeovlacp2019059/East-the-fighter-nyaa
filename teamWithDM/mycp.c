5281d841efbaa6b165c0367aa684df90760179e2
π2019059 <72515596+xar1sgeovlacp2019059@users.noreply.github.com>
2021-11-15 12:24:29 +0200

    Update mycp.c

--- teamWithDM/mycp.c
+++ teamWithDM/mycp.c
@@ -5,21 +5,23 @@
 
 
 int main(int argc, char **argv){ // input: argc=argument counter, argv=argument vector, argv[0] is the name of the program, argv[1] is the name of first file the first file
+ int i=1; // identifier
  char ch;
  FILE *fp;
  FILE *gp;
- if(argc <2){
+ 
+ if(argc <i+1){
  printf("give a filename\n");
  return 1;
  }
 
  if (access(argv[1], F_OK)== -1){ //file does not exist
-  printf("file %s does not exist\n",argv[1]); //file does not exist
+  printf("file %s does not exist\n",argv[i]); //file does not exist
 
  }
 
- fp=fopen(argv[1],"r"); // open the file to read
- gp=fopen(argv[2],"w");
+ fp=fopen(argv[i],"r"); // open the file to read
+ gp=fopen(argv[i+1],"w");
  while((ch = fgetc(fp)) != EOF){ //until end of file
    fprintf(gp,"%c",ch);// show every char
  }
