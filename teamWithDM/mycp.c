90cd30ddeec3ab401d8da36facc78c44eb7bf262
π2019059 <72515596+xar1sgeovlacp2019059@users.noreply.github.com>
2021-11-17 20:40:30 +0200

    Update mycp.c

--- teamWithDM/mycp.c
+++ teamWithDM/mycp.c
@@ -6,7 +6,7 @@
 #include <time.h>
 
 int mode(int i, char* cbuff){
- if ((cbuff[0]=='-') && (cbuff[1]=='b')){
+ if ((cbuff[0]=='-') && (cbuff[1]=='b')){ // there we search if the mode is buffer mode
   i=i+1; 
  }
  return i;
@@ -15,8 +15,8 @@ int mode(int i, char* cbuff){
 int buffsize(char* cbuff){
 
  int buffsize;
- cbuff = memmove(cbuff, cbuff+2, strlen(cbuff));
- buffize = atoi(cbuff);
+ cbuff = memmove(cbuff, cbuff+2, strlen(cbuff)); // here we remove -b 
+ buffize = atoi(cbuff); // make string to int
  
  return buffsize;
 }
@@ -25,7 +25,7 @@ int buffsize(char* cbuff){
 int main(int argc, char **argv){ // input: argc=argument counter, argv=argument vector, argv[0] is the name of the program, argv[1] is the name of first file the first file
 
  clock_t timing;
- int i=1; // identifier
+ int i=1; // identifier if i=2 -> buffer mode
  char* buff;
  int buffsize;
  int file1;
@@ -37,35 +37,36 @@ int main(int argc, char **argv){ // input: argc=argument counter, argv=argument 
  FILE *gp;
  
 
- i=mode(i,argv[1]);
+ i=mode(i,argv[1]); // the identifier(i) will show us if there is buffer mode or not
  if(i==2){
  
- buffsize=buffmode(argv[1]);
+ buffsize=buffmode(argv[1]); // we set size of custom buffer
  buff=malloc(sizeof(char) * buffsize);
  }
  
  
- if(argc <i+1){
+ if(argc <i+1){ // you forgot the name of file
  printf("give a filename\n");
  return 1;
  }
  
  if (access(argv[i], F_OK)== -1){ //file does not exist
-  printf("file %s does not exist\n",argv[i]); //file does not exist
+  printf("file %s does not exist\n",argv[i]); 
   return 2;
  }
 
- if(i==2){
-  file1=open(argv[i], O_RDONLY);
-  file2=open(argv[i+1], O_WRONLY | O_CREAT , 0644);
+ if(i==2){ // copy the file with buffer mode
+  file1=open(argv[i], O_RDONLY); // readonly
+  file2=open(argv[i+1], O_WRONLY | O_CREAT , 0644); // write only | create if not exist write&read-read-read(owner-group-others)
  
   timing=clock();// time start
-  while((readchar=read(file1,buff,buffsize)) > 0){
-   if((write(file2, buff, readchar)) > readchar){
+  
+  while((readchar=read(file1,buff,buffsize)) > 0){ // read with buffer size
+   if((write(file2, buff, readchar)) > readchar){ // write with buffer
     
-    return 0;
+    return 0;// everything is ok
    }
-   bytes=bytes+readchar;
+   bytes=bytes+readchar; //count bytes
   }
   bytes=bytes-1;
   timing=clock() - timing;//timer stop
@@ -78,7 +79,7 @@ int main(int argc, char **argv){ // input: argc=argument counter, argv=argument 
   fp=fopen(argv[i],"r"); // open the file to read
   gp=fopen(argv[i+1],"w");
   while((ch = fgetc(fp)) != EOF){ //until end of file
-   fprintf(gp,"%c",ch);// show every char
+   fprintf(gp,"%c",ch);
   }
   fclose(gp);
   fclose(fp);
