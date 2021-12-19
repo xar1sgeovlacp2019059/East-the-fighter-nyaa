a3c723b3cfc5d310edd464e3c3c014fa0646dc8a
π2019059 <72515596+xar1sgeovlacp2019059@users.noreply.github.com>
2021-12-19 10:31:41 -0800

    Update banana.c

--- teamWithDM/banana.c
+++ teamWithDM/banana.c
@@ -6,81 +6,67 @@
 #include <sys/wait.h>
 #include <time.h>
 
-int main(int argc, char* argv[]){
+
+int main(int argc, char* argv[]) {
+
  int fd[2]; 
- if (pipe(fd) ==-1){
-  return 1;
- }
- int pid1 = fork();
- if (pid1 <0 ){
-  return 2; 
- }
+ char buffer[4096];
+  if (argc >4){
+   return 3;
+  }
+  if (pipe(fd) ==-1){
+   printf("problem occured while opening the file");
+   return 1;
+  }
  
- if (pid1 == 0){ // process(child) cat
-  dup2(fd[1], STDOUT_FILENO);// 
-  close(fd[0]);
-  close(fd[1]);
-  
-  char* fl1 = (char*)NULL;// 
-  int len1 = strlen(argv[1]); 
-  
-  if ((fl1 = malloc(len1+1)) != NULL){
-    bzero(fl1, len1+1);
-    strncpy(fl1, argv[1], len1);
-   }
-  execlp("cat","cat",fl1, NULL);
- 
- }
- 
- int  pid2 =fork();
- if (pid2 <0){
-  return 3;
- }
- 
- if (pid2 ==0){
-  dup2(fd[0], STDIN_FILENO);
-  close(fd[0]);
-  close(fd[1]);
-  // new
+  int  pid2 =fork();// create a child procces
+  if (pid2 <0){
+   return 2; // if pid2==-1 
+  }
+  if (pid2 ==0){ // child procces 
+   dup2(fd[1],STDOUT_FILENO);// parent takes std_out of child
+   close(fd[0]);// we don't need to read or write anithing
+   close(fd[1]);
    char *ar1=malloc(sizeof(char)+1);
    char *ar2=malloc(sizeof(char)+1);
-   char *full=malloc(sizeof(char)+2);
+   char *full=malloc(sizeof(char)+2);// from argv to string cause sed needs them
    char *file=malloc(sizeof(char)+1);
-   strcpy(full,"s/");
-   char *buffer;
-   char command[512];
+   strcpy(full,"s/");// sizeof(char)+2
+   char *buf; //another buffer, but for certain work
    int i = 1;
    for(i = 1; i < argc; i++){
-     buffer = malloc(strlen(argv[i]) + 5);
-     strcpy(buffer,argv[i]);
+     buf = malloc(strlen(argv[i]) + 5);
+     strcpy(buf,argv[i]);
      if(i==1){
-      strcpy(file,buffer);
+      strcpy(file,buf);
      }else if(i==2){
-      strcat(buffer,"/");
-      strcpy(ar1,buffer);
+      strcat(buf,"/");
+      strcpy(ar1,buf);
       
      }else if(i==3){
-      strcat(buffer,"/g");
-      strcpy(ar2,buffer);
+      strcat(buf,"/g");
+      strcpy(ar2,buf);
      }
      
-     free(buffer);
+     free(buf);
   }
    //printf("%s, %s, %s\n",full,ar1, ar2);
    strcat(ar1,ar2);
    //printf("%s \n",ar1);
    free(ar2);
    strcat(full,ar1);
+   execlp("sed", "sed","-e","s/^/Data received through pipe /g","-e",full,file, NULL);
    
-   //printf("%s \n",full);
-  //new  
-  execlp("sed", "sed","-e","s/^/Data received through pipe /g","-e",full,file, NULL);
- }
- close(fd[0]);
- close(fd[1]);
- 
- waitpid(pid1, NULL, 0);
- waitpid(pid2, NULL, 0);
- 
- return 0;
+  }else{
+   
+   close(fd[1]);
+   
+   read(fd[0],buffer,sizeof(buffer));// read the std_out and save it to buffer
+   printf("%s\n",buffer);
+   waitpid(pid2, NULL, 0); // wait for child procces to finish
+  }
+  
+  
+  
+  return 0;
 }
